f14d738d2add0fdce51c9bc376f7c6fa3cd46916
javatrainingse7 <javatraining.se7@gmail.com>
2015-08-16 13:39:02 +0530

    First Commit

---
+++ src/com/java/training/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.java.training;
+
+/**
+ * @author Pragim
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to java.................");
+
+	}
+
+}
